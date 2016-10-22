32826757b27c11410d697f6ad7a68fdc19f32427
Hector Coto Rojas <heavenhell64@gmail.com>
2016-10-21 22:36:07 -0600

    Some changes

--- app/app.module.js
+++ app/app.module.js
@@ -1,9 +1,27 @@
-var app = angular.module('App', []);
+(function() {
 
-app.controller("home", function($scope) {
+	var app = angular.module('App', []);
 
-});
+	app.controller("home", function($scope) {
 
-app.controller("searchBar", function($scope) {
-	
-});
+	});
+
+	app.controller("searchBar", function($scope) {
+
+	});
+
+	app.controller("repoInfo", function($scope, $http) {
+		$http.get('https://api.github.com/users/tora1024')
+			 .then(successCallback, errorCallback);
+
+		var successCallback = function(response) {
+			console.log("data: " + response.data);
+			$scope.data = response.data;
+		};
+
+		var errorCallback = function(response) {
+			console.error('Gists error', response.status, response.data);
+		};
+	});
+
+})();
